db69e367d1b59299ace88019c712221db7c34807
juanmi <juanminet@gmail.com>
2011-07-12 19:05:48 +0200

    Add Accessor Mock

--- phoneGapCalendarAPI/application/assets/www/calendar.js
+++ phoneGapCalendarAPI/application/assets/www/calendar.js
@@ -1,140 +1,167 @@
-var deviceInfo = function() {
-    document.getElementById("platform").innerHTML = device.platform;
-    document.getElementById("version").innerHTML = device.version;
-    document.getElementById("uuid").innerHTML = device.uuid;
-    document.getElementById("name").innerHTML = device.name;
-    document.getElementById("width").innerHTML = screen.width;
-    document.getElementById("height").innerHTML = screen.height;
-    document.getElementById("colorDepth").innerHTML = screen.colorDepth;
-};
+var CalendarEvent = function (	id, description, location, summary, start, end, status, transparency, recurrence, reminder){
+	this.id = id || null;
+	this.description = description || null;
+	this.location = location || null;
+	this.summary = summary || null;
+	this.start = start || null;
+	this.end = end || null;
+	this.status = status || null;
+	this.transparency = transparency || null;
+	this.recurrence = recurrence || null;
+	this.reminder = reminder || null;
+}
+								
 
-var getLocation = function() {
-    var suc = function(p) {
-        alert(p.coords.latitude + " " + p.coords.longitude);
-    };
-    var locFail = function() {
-    };
-    navigator.geolocation.getCurrentPosition(suc, locFail);
-};
 
-var beep = function() {
-    navigator.notification.beep(2);
-};
 
-var vibrate = function() {
-    navigator.notification.vibrate(0);
-};
 
-function roundNumber(num) {
-    var dec = 3;
-    var result = Math.round(num * Math.pow(10, dec)) / Math.pow(10, dec);
-    return result;
+
+
+
+var CalendarRepeatRule = function ( frecuency, interval, expires, exceptionDates, daysInWeek, daysInMonth, daysInYear, weeksInMonth, monthsInYear){
+	this.frecuency = frecuency || null;
+	this.interval = interval || null;
+	this.expires = expires || null;
+	this.exceptionDates = exceptionDates || null;
+	this.daysInWeek = daysInWeek || null;
+	this.daysInMonth = daysInMonth || null;
+	this.daysInYear = daysInYear || null;
+	this.weeksInMonth = weeksInMonth || null;
+	this.monthInYear = monthsInYear || null;	
+}	
+
+
+
+
+var CalendarFindOptions = function (filter, multiple){
+	  this.filter = filter || '';
+	  this.multiple = multiple || true;	  
 }
 
-var accelerationWatch = null;
 
-function updateAcceleration(a) {
-    document.getElementById('x').innerHTML = roundNumber(a.x);
-    document.getElementById('y').innerHTML = roundNumber(a.y);
-    document.getElementById('z').innerHTML = roundNumber(a.z);
+
+var CalendarEventfilter = function (startBefore, startAfter, endBefore, endAfter){
+	this.startBefore = startBefore;
+	this.startAfter = startAfter;
+	this.endBefore = endBefore;
+	this.endAfter = endAfter;
 }
 
-var toggleAccel = function() {
-    if (accelerationWatch !== null) {
-        navigator.accelerometer.clearWatch(accelerationWatch);
-        updateAcceleration({
-            x : "",
-            y : "",
-            z : ""
-        });
-        accelerationWatch = null;
-    } else {
-        var options = {};
-        options.frequency = 1000;
-        accelerationWatch = navigator.accelerometer.watchAcceleration(
-                updateAcceleration, function(ex) {
-                    alert("accel fail (" + ex.name + ": " + ex.message + ")");
-                }, options);
+
+/**
+ *  CalendarError.
+ *  An error code assigned by an implementation when an error has occurreds
+ * @constructor
+ */
+var CalendarError = function() {
+    this.code=null;
+};
+
+/**
+ * Error codes
+ */
+CalendarError.UNKNOWN_ERROR = 0;
+CalendarError.INVALID_ARGUMENT_ERROR = 1;
+CalendarError.TIMEOUT_ERROR = 2;
+CalendarError.PENDING_OPERATION_ERROR = 3;
+CalendarError.IO_ERROR = 45;
+CalendarError.NOT_SUPPORTED_ERROR = 5;
+CalendarError.PERMISSION_DENIED_ERROR = 20;
+
+
+
+
+var Calendar = function() { 
+
+}
+
+
+/**
+* This function creates a new event, but it does not persist the event
+* to device storage. To persist the event to device storage, invoke
+* calendar.save().
+* @param properties an object who's properties will be examined to create a new Contact
+* @returns new Contact object
+*/
+Calendar.prototype.create = function(properties) {
+    var i;
+	var event = new CalendarEvent();
+    for (i in properties) {
+        if (event[i] !== 'undefined') {
+        	event[i] = properties[i];
+        }
+    }
+    return event;
+};
+
+/**
+* Removes calendar from device storage.
+* @param successCB success callback
+* @param errorCB error callback
+*/
+CalendarEvent.prototype.remove = function(successCB, errorCB) {
+    if (this.id === null) {
+        var errorObj = new ContactError();
+        errorObj.code = ContactError.NOT_FOUND_ERROR;
+        errorCB(errorObj);
+    }
+    else {
+        PhoneGap.exec(successCB, errorCB, "CalendarPlugin", "remove", [this.id]);
     }
 };
 
-var preventBehavior = function(e) {
-    e.preventDefault();
+
+/**
+* Persists calendar to device storage.
+* @param successCB success callback
+* @param errorCB error callback
+*/
+CalendarEvent.prototype.save = function(successCB, errorCB) {		
+    PhoneGap.exec(successCB, errorCB, "CalendarPlugin", "save", [this]);
 };
 
-function dump_pic(data) {
-    var viewport = document.getElementById('viewport');
-    console.log(data);
-    viewport.style.display = "";
-    viewport.style.position = "absolute";
-    viewport.style.top = "10px";
-    viewport.style.left = "10px";
-    document.getElementById("test_img").src = "data:image/jpeg;base64," + data;
-}
 
-function fail(msg) {
-    alert(msg);
-}
-
-function show_pic() {
-    navigator.camera.getPicture(dump_pic, fail, {
-        quality : 50
-    });
-}
-
-function close() {
-    var viewport = document.getElementById('viewport');
-    viewport.style.position = "relative";
-    viewport.style.display = "none";
-}
-
-// This is just to do this.
-function readFile() {
-    navigator.file.read('/sdcard/phonegap.txt', fail, fail);
-}
-
-function writeFile() {
-    navigator.file.write('foo.txt', "This is a test of writing to a file",
-            fail, fail);
-}
-
-function contacts_success(contacts) {
-    alert(contacts.length
-            + ' contacts returned.'
-            + (contacts[2] && contacts[2].name ? (' Third contact is ' + contacts[2].name.formatted)
-                    : ''));
-}
-
-function get_contacts() {
-    var obj = new ContactFindOptions();
-    obj.filter = "";
-    obj.multiple = true;
-    obj.limit = 5;
-    navigator.service.contacts.find(
-            [ "displayName", "name" ], contacts_success,
-            fail, obj);
-}
-
-var networkReachableCallback = function(reachability) {
-    // There is no consistency on the format of reachability
-    var networkState = reachability.code || reachability;
-
-    var currentState = {};
-    currentState[NetworkStatus.NOT_REACHABLE] = 'No network connection';
-    currentState[NetworkStatus.REACHABLE_VIA_CARRIER_DATA_NETWORK] = 'Carrier data connection';
-    currentState[NetworkStatus.REACHABLE_VIA_WIFI_NETWORK] = 'WiFi connection';
-
-    confirm("Connection type:\n" + currentState[networkState]);
+/**
+* This function returns and array of events of calendar.  It is required as we need to convert raw
+* JSON objects into concrete Calendar objects.  Currently this method is called after
+* navigator.service.calendar.find but before the find methods success call back.
+*
+* @param jsonArray an array of JSON Objects that need to be converted to Contact objects.
+* @returns an array of Contact objects
+*/
+Calendar.prototype.cast = function(pluginResult) {
+	var events = [];
+	var i;
+	for (i=0; i<pluginResult.message.length; i++) {
+		events.push(window.plugins.calendar.create(pluginResult.message[i]));
+	}
+	pluginResult.message = events;
+	return pluginResult;
+};
+/**
+* @param options The options for the search
+* @param successCallback The callback which will be called when calendar is successful
+* @param failureCallback The callback which will be called when calendar encouters an error
+*/
+Calendar.prototype.find = function(options,successCallback, failureCallback) {
+	
+   return PhoneGap.exec(successCallback,    //Callback which will be called when calendar is successful
+   					failureCallback,     //Callback which will be called when calendar encounters an error
+   					'CalendarPlugin',  //Telling PhoneGap that we want to run "Calendar" Plugin
+   					'find',              //Telling the plugin, which action we want to perform
+   					[options]);        //Passing a list of arguments to the plugin
 };
 
-function check_network() {
-    navigator.network.isReachable("www.mobiledevelopersolutions.com",
-            networkReachableCallback, {});
-}
-
-function init() {
-    // the next line makes it impossible to see Contacts on the HTC Evo since it
-    // doesn't have a scroll button
-    // document.addEventListener("touchmove", preventBehavior, false);
-    document.addEventListener("deviceready", deviceInfo, true);
-}
+/**
+* <ul>
+* <li>Register the Calendar Javascript plugin.</li>
+* <li>Also register native call which will be called when this plugin runs</li>
+* </ul>
+*/
+PhoneGap.addConstructor(function() {
+	//Register the javascript plugin with PhoneGap
+	PhoneGap.addPlugin('calendar', new Calendar());
+	
+	//Register the native class of plugin with PhoneGap
+	PluginManager.addService("CalendarPlugin","com.trial.phonegap.plugin.calendar.CalendarPlugin");
+});
---
+++ phoneGapCalendarAPI/application/src/com/trial/phonegap/plugin/calendar/CalendarAccessorMock.java
@@ -0,0 +1,222 @@
+package com.trial.phonegap.plugin.calendar;
+
+
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import android.app.Activity;
+import android.util.Log;
+import android.webkit.WebView;
+
+public class CalendarAccessorMock extends CalendarAccessorCreator{
+	private static final String LOG_TAG = "[Android:CalendarAccesorMock.java]";
+	static List<JSONObject> calendar1; 
+	
+	static{
+	
+
+	}
+	 /**
+     * Create an contact accessor.
+     */
+    public CalendarAccessorMock(WebView view, Activity app) {
+		mApp = app;
+		mView = view;
+		
+		init();
+	}
+    
+	private void init() {
+	calendar1 = new ArrayList<JSONObject>();
+		
+	
+		String description= "Meeting with Joe's team";
+		String location= "Conf call #+4402000000001";
+		String summary= "Agenda:\\n\\n\\t* Introductions\\n\\t* AoB";
+		String start= "2011-03-24 09:00:00";
+		String end= "2011-03-24 10:00:00";
+		String status= "pending";
+		String transparency= "transparent";		
+		String reminder= "2011-03-24T13:00:00+00:00";
+		String frequency= "yearly";
+		JSONArray daysInMonth = (new JSONArray()).put(24);
+		JSONArray monthsInYear = (new JSONArray()).put(3);
+		String expires = "2011-06-11T16:00:00Z";
+		addCalendar(calendar1, description, location, summary, start,
+				end, status, transparency, reminder, 
+				frequency, daysInMonth, monthsInYear, expires);
+		
+		
+		description= "Meeting with dentist";
+		location= "Conf call #+222222222222";
+		summary= "Agenda:\\n\\n\\t* Introductions\\n\\t* AoB";
+		start= "2011-04-24 09:00:00";
+		end= "2011-04-24 10:00:00";
+		status= "pending";
+		transparency= "transparent";		
+		reminder= "2011-04-24T13:00:00+00:00";
+		frequency= "monthly";
+		daysInMonth = (new JSONArray()).put(25);
+		monthsInYear = (new JSONArray()).put(4);
+		expires = "2011-06-11T16:00:00Z";
+		addCalendar(calendar1, description, location, summary, start,
+				end, status, transparency, reminder, 
+				frequency, daysInMonth, monthsInYear, expires);
+		
+	}
+
+	@Override
+	public JSONArray find(JSONObject options) {	
+		Date dateAfter = null;
+		Date dateBefore = null;
+		JSONArray events = new JSONArray();	
+		try {
+			
+			JSONObject filter = options.getJSONObject("filter");	
+			Log.i(LOG_TAG,"Date After: " + filter.getString("startAfter"));
+			Log.i(LOG_TAG,"Date Before: " + filter.getString("startBefore"));			
+			dateAfter = stringToDate(filter.getString("startAfter"));
+			dateBefore = stringToDate(filter.getString("startBefore"));
+			
+			for (JSONObject event : calendar1){
+				if (stringToDate(event.getString("start")).after(dateAfter) &&
+					stringToDate(event.getString("start")).before(dateBefore)){
+					events.put(event);
+				}
+			}
+			
+		
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		
+		return events;	
+		
+	}
+	
+	@Override
+	public boolean save(JSONObject newCalendarEvent) {
+		try {
+			if (exists(newCalendarEvent)) {
+				calendar1.set(	Integer.parseInt(newCalendarEvent.getString("id")),
+							  	newCalendarEvent);
+			} else {
+				calendar1.add(newCalendarEvent);
+				newCalendarEvent.put("id", String.valueOf(calendar1.indexOf(newCalendarEvent)));
+			}
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	private boolean exists(JSONObject eventCalendar){
+	
+		try {
+			
+			if ((eventCalendar.isNull("id")) ||
+				(Integer.parseInt(eventCalendar.getString("id")) >= calendar1.size()) || 
+				(calendar1.get(Integer.parseInt(eventCalendar.getString("id"))) == null)){
+				return false;
+			}
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return false;
+		} 
+		return true;
+		
+	}
+	
+	private static void addCalendar(List<JSONObject> calendar,									
+									String description,
+									String location,
+									String summary,
+									String start,
+									String end,
+									String status,
+									String transparency,
+									String reminder,
+									String frequency,
+									JSONArray daysInMonth,
+									JSONArray monthsInYear,
+									String expires){
+		
+		JSONObject event = new JSONObject();
+		JSONObject recurrence= new JSONObject();
+		try {
+			recurrence.put("frequency", frequency);
+			recurrence.put("daysInMonth", daysInMonth);
+			recurrence.put("monthsInYear", monthsInYear);
+			recurrence.put("expires", expires);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {			
+			event.put("description", description);
+			event.put("location", location);		
+			event.put("summary", summary);
+			event.put("start", start);
+			event.put("end", end);
+			event.put("status", status);
+			event.put("transparency", transparency);
+			event.put("reminder", reminder);
+			event.put("recurrence", recurrence);
+			
+			calendar.add(event);
+			event.put("id", String.valueOf(calendar.indexOf(event)));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static Date stringToDate(String dateString) {
+		Date date = null;
+
+		SimpleDateFormat dformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		try {
+			date = dformat.parse(dateString);
+			//its normal having an parsing exception here, for instance, with "No Credit" as string
+		} catch (ParseException parseException) {
+			Log.i(LOG_TAG, parseException.getMessage());
+			return null;			
+		}
+		return date;
+	}
+	
+	/**
+	 * Parse any date into string format yyyy/MM/dd-HH:mm:ss 
+	 * @param date
+	 * @return String date
+	 */
+	public static String dateToString(Date date){		
+		SimpleDateFormat dformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return dformat.format(date);
+		
+	}
+
+	
+
+}
